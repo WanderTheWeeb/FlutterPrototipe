41147b4f1c5f3f47f7a48f0f978a174aa8e0452a
Arrttyy <ironman60@live.com.mx>
2023-11-16 20:10:18 -0600

    se modifico rooms page

--- lib/pages/rooms_page.dart
+++ lib/pages/rooms_page.dart
@@ -1,4 +1,4 @@
-import 'package:MindWell/pages/register_page.dart';
+
 import 'package:MindWell/utils/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
